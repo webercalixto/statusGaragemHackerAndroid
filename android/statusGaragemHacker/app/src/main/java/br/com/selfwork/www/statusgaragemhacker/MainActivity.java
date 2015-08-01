1b1ffeed0a185f7cc8ede7810332522da971a522
Weber Calixto - PC Draco <webersouzacalixto@gmail.com>
2015-08-01 12:04:54 -0300

    Primeira versao estavel do aplicativo

--- android/statusGaragemHacker/app/src/main/java/br/com/selfwork/www/statusgaragemhacker/MainActivity.java
+++ android/statusGaragemHacker/app/src/main/java/br/com/selfwork/www/statusgaragemhacker/MainActivity.java
@@ -1,18 +1,60 @@
 package br.com.selfwork.www.statusgaragemhacker;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class MainActivity extends Activity {
-
+    TextView estado;
+    thStatus status = new thStatus();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        estado=(TextView)findViewById(R.id.estado);
+        estado.setText("Perguntao ao garagem se está aberto....");
+        status.execute("");
+
     }
+
+
+    private class thStatus extends AsyncTask<String, Void, String> {
+        String retorno;
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                URL url = new URL("http://garagemhacker.org/status.txxt");
+                BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+                String str;
+                while ((str = in.readLine()) != null) {
+                    retorno = str;
+                }
+                in.close();
+            } catch (MalformedURLException e) {
+                retorno = "erro!";
+            } catch (IOException e) {
+                retorno = "erro!";
+            }
+            return "ok";
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            retorno = retorno.substring(0, 1).toUpperCase() + retorno.substring(1);
+            estado.setText(retorno);
+        }
+    }
+
 
 
     @Override
@@ -36,4 +78,6 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
